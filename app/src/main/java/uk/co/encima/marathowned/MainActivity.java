e5f9edd5b59e79bce9f8c7641947a6719bd5ef9a
Chris Gwilliams <Chris Gwilliams>
2016-04-16 11:09:33 +0100

    initial commit

---
+++ app/src/main/java/uk/co/encima/marathowned/MainActivity.java
@@ -0,0 +1,26 @@
+package uk.co.encima.marathowned;
+
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @TargetApi(Build.VERSION_CODES.M)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void btnClick(View view) {
+        Uri playlistUri = Uri.parse(view.getTag().toString());
+        Intent i = new Intent(Intent.ACTION_VIEW, playlistUri);
+        startActivity(i);
+    }
+}
